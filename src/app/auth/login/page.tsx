7bb3be7a8e2c310bdb1ee3792cb3a3462ae6ae8d
Ademu Anthony <ademuanthony@gmail.com>
2025-09-10 06:40:47 +0100

    no oauth

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -240,17 +240,6 @@ function LoginContent() {
                 )}
               </button>
             </div>
-
-            <div className="mt-6">
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-gray-300" />
-                </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-white text-gray-500">Or continue with</span>
-                </div>
-              </div>
-            </div>
           </form>
         </div>
       </div>
